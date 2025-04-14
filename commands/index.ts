353051b8ea0dfcf02e782c99248817d52437bae8
ronaldarndt <ronaldarndtf@gmail.com>
2025-04-13 22:45:36 -0300

    remove unused commands

--- commands/index.ts
+++ commands/index.ts
@@ -1,11 +1,9 @@
 import * as ping from "./ping";
 import * as say from "./say";
-import * as play from "./play";
 import * as amp from "./amp";
 
 export const commands = {
   ping,
   say,
-  play,
   amp
 };
